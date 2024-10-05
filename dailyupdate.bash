f3035b8469029acd056126ece169639ced6d27ae
Patrick Cahill <98288865+PatrickhCahill@users.noreply.github.com>
2024-10-06 07:21:19 +1100

    Daily update

--- dailyupdate.bash
+++ dailyupdate.bash
@@ -5,7 +5,7 @@ cd `dirname $0`
 
 jupyter execute src/polling_vals.ipynb 
 jupyter execute src/model_and_plots.ipynb
-jupyter execute src/polling_vals
+jupyter execute src/facts_over_time.ipynb
 
 git add -A
 git commit -m "Daily update"
